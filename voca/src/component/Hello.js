c8a29846964fa6664fe8e9d33045b2f183eb21f6
postforty <75767073+postforty@users.noreply.github.com>
2023-02-13 22:54:24 +0900

    React JS #6 이벤트 처리(Handling Events) - 초보자를 위한 리액트 강좌

--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -1,9 +1,20 @@
 import World from "./World";
+import styles from "./Hello.module.css";
 
 const Hello = () => (
   <>
-    <p>Hello</p>
+    <p
+      style={{
+        color: "#f00",
+        borderRight: "12px solid #000",
+        marginBottom: "50px",
+        opacity: 0.1,
+      }}
+    >
+      Hello
+    </p>
     <World />
+    <div className={styles.box}>Hello</div>
   </>
 );
 export default Hello;
