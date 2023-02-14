f038e1c74c3e2c7a8ec76d4e0095c4de02e6bb68
postforty <75767073+postforty@users.noreply.github.com>
2023-02-14 23:25:07 +0900

    React JS #7 state, useState - 초보자를 위한 리액트 강좌

--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -1,6 +1,16 @@
 import World from "./World";
 import styles from "./Hello.module.css";
 
+function showName() {
+  console.log("Mike");
+}
+function showAge(age) {
+  console.log(age);
+}
+function showText(txt) {
+  console.log(txt);
+}
+
 const Hello = () => (
   <>
     <p
@@ -13,6 +23,33 @@ const Hello = () => (
     >
       Hello
     </p>
+    <button onClick={showName}>Show name</button>
+    <button
+      onClick={() => {
+        showAge(40);
+      }}
+    >
+      {/* <button
+      onClick={() => {
+        console.log(40);
+      }}
+    > */}
+      Show age
+    </button>
+    <input
+      type="text"
+      onChange={(e) => {
+        const txt = e.target.value;
+        showText(txt);
+      }}
+    />
+    {/* <input
+      type="text"
+      onChange={(e) => {
+        console.log(e.target.value);
+      }}
+    /> */}
+    {/* <input type="text" onChange={showText} /> */}
     <World />
     <div className={styles.box}>Hello</div>
   </>
