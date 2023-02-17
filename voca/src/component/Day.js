0c9132573dcec60d2090c69faf628f1542dab961
postforty <75767073+postforty@users.noreply.github.com>
2023-02-17 20:05:33 +0900

    React JS #10 라우터 구현 react-router-dom - 초보자를 위한 리액트 강좌

--- voca/src/component/Day.js
+++ voca/src/component/Day.js
@@ -1,11 +1,15 @@
 import dummy from "../db/data.json";
 
 export default function Day() {
+  const day = 1;
+  const wordList = dummy.words.filter((word) => word.day === day);
+  console.log(wordList);
+
   return (
     <>
       <table>
         <tbody>
-          {dummy.words.map((word) => (
+          {wordList.map((word) => (
             <tr key={word.id}>
               <td>{word.eng}</td>
               <td>{word.kor}</td>
@@ -16,3 +20,5 @@ export default function Day() {
     </>
   );
 }
+
+// 6:14 React JS #9 더미 데이터 구현, map() 반복문 - 초보자를 위한 리액트 강좌
