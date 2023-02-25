49704e9b00ee1f81adafe25d4e12da1884ac7bb5
postforty <75767073+postforty@users.noreply.github.com>
2023-02-25 21:45:54 +0900

    React JS #11 json-server, REST API - 초보자를 위한 리액트 강좌

--- voca/src/component/Day.js
+++ voca/src/component/Day.js
@@ -1,6 +1,6 @@
 import dummy from "../db/data.json";
 import { useParams } from "react-router-dom";
-import Word from "./word";
+// import Word from "./word";
 
 export default function Day() {
   const day = useParams().day;
@@ -12,7 +12,17 @@ export default function Day() {
       <table>
         <tbody>
           {wordList.map((word) => (
-            <Word word={word} key={word.id} />
+            <tr key={word.id}>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>{word.eng}</td>
+              <td>{word.kor}</td>
+              <td>
+                <button>뜻 보기</button>
+                <button class="btn_del">삭제</button>
+              </td>
+            </tr>
           ))}
         </tbody>
       </table>
