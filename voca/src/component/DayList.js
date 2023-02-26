cd41b5bd5e85afecb61220f8e2cc53f6d0f7fb88
postforty <75767073+postforty@users.noreply.github.com>
2023-02-26 21:53:48 +0900

    React JS #12 useEffect, fetch()로 API 호출 - 초보자를 위한 리액트 강좌

--- voca/src/component/DayList.js
+++ voca/src/component/DayList.js
@@ -1,15 +1,28 @@
-import dummy from "../db/data";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function DayList() {
-  console.log(dummy);
+  const [days, setDays] = useState([]);
+  const [count, setCount] = useState(0);
+
+  function onClick() {
+    setCount(count + 1);
+  }
+
+  useEffect(() => {
+    console.log("Count change");
+  });
+
   return (
-    <ul className="list_day">
-      {dummy.days.map((day) => (
-        <li key={day.id}>
-          <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="list_day">
+        {days.map((day) => (
+          <li key={day.id}>
+            <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+          </li>
+        ))}
+      </ul>
+      <button onClick={onClick}>{count}</button>
+    </>
   );
 }
