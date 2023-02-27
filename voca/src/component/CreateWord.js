6f118f1831c20e82cf27fd80d3898046496d06ec
postforty <75767073+postforty@users.noreply.github.com>
2023-02-27 21:36:33 +0900

    React JS #13 Custom Hooks - 초보자를 위한 리액트 강좌

---
+++ voca/src/component/CreateWord.js
@@ -0,0 +1,28 @@
+import useFetch from "../hooks/useFetch";
+
+export default function CreateWord() {
+  const days = useFetch("http://localhost:3001/days");
+  return (
+    <form>
+      <div className="input_area">
+        <label>Eng</label>
+        <input type="text" placeholder="computer" />
+      </div>
+      <div className="input_area">
+        <label>Kor</label>
+        <input type="text" placeholder="컴퓨터" />
+      </div>
+      <div className="input_area">
+        <label>Day</label>
+        <select>
+          {days.map((day) => (
+            <option key={day.id} value={day.day}>
+              {day.day}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button>저장</button>
+    </form>
+  );
+}
